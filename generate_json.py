3d4c02eb10a88f27ae563cadb8128f609e6b4be6
Natsu- <romain.garrigues.cs@gmail.com>
2014-03-21 15:48:40 +0100

    Script to convert csv results file to json, needed by SIG part

---
+++ generate_json.py
@@ -0,0 +1,52 @@
+#-*- coding: utf-8 -*-
+#!/usr/bin/python
+
+from collections import namedtuple
+import csv
+import json
+import sys
+
+
+"""
+    Elisabeth Belaubre (soutenue par le Rassemblement citoyen),
+    Ahmed Chouki (soutenu par le NPA),
+    Pierre Cohen (PS, PCF, PRG, MDC, Parti occitan),
+    Serge Laroze (Toulouse Bleu Marine, soutenu par le FN),
+    Antoine Maurice (EÉLV),
+    Jean-Luc Moudenc (UMP, UDI, Nouveau centre, Modem, MPF),
+    Jean-Pierre Plancade (divers, centre gauche),
+    Jean-Christophe Sellin (Parti de Gauche-Front de Gauche),
+    Sandra Torremocha (Lutte ouvrière),
+    Christine de Veyrac (divers, centre droit).
+"""
+candidates = ['belaubre', 'chouki', 'cohen', 'laroze', 'maurice', 'moudenc',
+              'plancade', 'sellin', 'torremocha', 'deveyrac']
+OfficeResults = namedtuple(
+    'OfficeResult',
+    'name, associant, ' + ' ,'.join(candidates))
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 3:
+        msg = 'Usage : {command} <input.csv> <output.json>'.format(
+            command=sys.argv[0])
+        print(msg)
+        exit()
+
+    offices = []
+    with open(sys.argv[1], 'r') as csv_file:
+
+        reader = csv.reader(csv_file, delimiter=',')
+        # Skipping csv header
+        reader.next()
+
+        for row in map(OfficeResults._make, reader):
+
+            # Get results for each political list
+            results = [{candidate: getattr(row, candidate)}
+                       for candidate in candidates]
+            offices.append({row.associant: results})
+
+    with open(sys.argv[2], 'w') as outfile:
+        response_json = json.dump(offices, outfile)
